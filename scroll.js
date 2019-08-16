202cab6f2e7bebb4f5f02fc7b027966ed27e00c9
chenyaoming <chenyaoming@lanjinrong.com>
2019-08-16 18:24:27 +0800

    feat: add scroll.js

---
+++ scroll.js
@@ -0,0 +1,35 @@
+!function() {
+  var _start = 0;
+  var _end = 100;
+  var _scrolling = false;
+
+  function myScrollTo(top) {
+    if (_scrolling) return;
+    _scrolling = true;
+    if (top <= 0)  top = 0;
+    animateScroll(top);
+  }
+
+  function SineEaseOut(t,b,c,d){
+    return c * Math.sin(t / d * ( Math.PI / 2 )) + b;
+  }
+
+  function animateScroll(top) {
+    window.requestAnimationFrame(function() {
+      var current = document.body.scrollTop || document.documentElement.scrollTop;
+      var diff = top - current;
+      if (_start < _end) {
+        _start += 1;
+        var next = SineEaseOut(_start, current, diff, _end);
+        window.scrollTo(0, next);
+        animateScroll(top);
+      } else {
+        window.scrollTo(0, top);
+        _start = 0;
+        _scrolling = false;
+      }
+    });
+  }
+
+  window.myScrollTo = myScrollTo;
+}();
