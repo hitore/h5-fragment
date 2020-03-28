a06cdca43bf7d452525015fd79139076b2a3f974
chenyaoming <15732685948@163.com>
2020-03-28 21:42:21 +0800

    modifly code & add note

--- my-promise.js
+++ my-promise.js
@@ -1,3 +1,9 @@
+/**
+ * promise/A+规范：
+ * 1.一个promise的当前状态只能是pending、fulfilled和rejected三种之一。状态改变只能是pending到fulfilled或者pending到rejected。状态改变不可逆。
+ * 2.promise的then方法接收两个可选参数，表示该promise状态改变时的回调(promise.then(onFulfilled, onRejected))。then方法返回一个promise，then 方法可以被同一个 promise 调用多次。
+ */
+
 function myPromise(fn) {
     let self = this;
     this.status = 'padding';
@@ -6,23 +12,18 @@ function myPromise(fn) {
     this.finallyVal = undefined;
     this.resolveList = [];
     this.rejectList = [];
-    this.finallyFn = () => {};
 
 
     function resolve(val) {
-        self.status = 'resolve';
+        self.status = 'fulfilled';
         self.resolveVal = val;
         self.resolveList.forEach(cb => cb());
-        self.finallyVal = val;
-        self.finallyFn();
     }
 
     function reject(val) {
-        self.status = 'reject';
+        self.status = 'rejected';
         self.rejectVal = val;
         self.rejectList.forEach(cb => cb());
-        self.finallyVal = val;
-        self.finallyFn();
     }
 
     try {
@@ -32,34 +33,31 @@ function myPromise(fn) {
     }
     
 }
-myPromise.prototype.then = function(fn) {
-    if (this.status === 'resolve') {
-        fn(this.resolveVal);
+myPromise.prototype.then = function(onFulfilled, onRejected) {
+    if (this.status === 'fulfilled') {
+        onFulfilled(this.resolveVal);
     }
     if (this.status === 'padding') {
         this.resolveList.push(() => {
-            fn(this.resolveVal);
+            onFulfilled(this.resolveVal);
+        });
+        this.rejectList.push(() => {
+            onRejected(this.rejectVal);
         });
     }
     return this;
 }
 
-myPromise.prototype.catch = function(fn) {
-    if (this.status === 'reject') {
-        fn(this.rejectVal);
+myPromise.prototype.catch = function(onRejected) {
+    if (this.status === 'rejected') {
+        onRejected(this.rejectVal);
     }
     if (this.status === 'padding') {
         this.rejectList.push(() => {
-            fn(this.rejectVal);
+            onRejected(this.rejectVal);
         });
     }
     return this;
-}
-
-myPromise.prototype.finally = function(fn) {
-    this.finallyFn = () => {
-        fn(this.finallyVal);
-    }
 }
 
 function foo() {
@@ -86,6 +84,3 @@ foo()
     .then(() => {
         console.log('more');
     })
-    .finally(res => {
-        console.log('finally', res);
-    });
